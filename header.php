79d22542591e6e4c04389521413a6558a7c219fb
Krista Hennings-Vinocur <krista.hennings@gmail.com>
2017-03-19 14:31:52 -0400

    add back the rendering of the menu for now

--- header.php
+++ header.php
@@ -55,7 +55,7 @@
                 </div>
             </div>
             <div class="top-bar-right">
-                <?php //foundationpress_top_bar_r(); ?>
+                <?php foundationpress_top_bar_r(); ?>
 
                 <?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
                     <?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
