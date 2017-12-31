145e8b28cf2c681c61f2d5ab707e064d650037e2
Krista Hennings-Vinocur <krista.hennings@gmail.com>
2017-12-31 07:43:25 -0500

    add a shopping cart to the header

--- header.php
+++ header.php
@@ -61,6 +61,19 @@
                 </div>
             </div>
             <div class="top-bar-right">
+                <!-- Shopping Cart Desktop -->
+                <?php if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+                 
+                    $count = WC()->cart->cart_contents_count;
+                    ?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php 
+                    if ( $count > 0 ) {
+                        ?>
+                        <span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>
+                        <?php
+                    }
+                        ?></a>
+                 
+                <?php } ?>
                 <?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
                     <?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
                 <?php endif; ?>
