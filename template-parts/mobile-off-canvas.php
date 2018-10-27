2bf3314fe473e39ec310e434079aa6a28a36c9d4
Krista Hennings-Vinocur <krista.hennings@gmail.com>
2018-10-27 11:38:49 -0400

    adding the cart stuff to the mobile menu

--- template-parts/mobile-off-canvas.php
+++ template-parts/mobile-off-canvas.php
@@ -13,6 +13,27 @@
 
   <?php foundationpress_mobile_nav(); ?>
 	<div class="login-reg">
+    <!-- Shopping Cart mobile -->
+    <?php if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+     
+        $count = WC()->cart->cart_contents_count;
+        ?>
+        <br/>
+        <span class="cart-icon-wrapper">
+            <a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>">
+            
+            <?php 
+            if ( $count > 0 ) {
+                ?>
+
+                <span class="cart-contents-count"><?php echo esc_html( $count ); ?> </span>
+                <?php
+            }
+                ?></a>
+            <span class="cart-icon"></span>
+        </span>
+     
+    <?php } ?>
 		<?php if ( is_user_logged_in() ) { ?>
           <a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" title="<?php _e('My Account','woothemes'); ?>"><?php _e('My Account','woothemes'); ?></a>
           <a href="/my-account/customer-logout">Log Out</a>
